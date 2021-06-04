6d8047d2b8ccaa207d5282c165661211186ee250
Fahad <fahadmahmood1200@gmail.com>
2021-06-04 17:24:03 +0530

    Last Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import ImageGrid from "./Components/ImageGrid";
+import Modal from "./Components/Modal";
 import UploadForm from "./Components/UploadForm";
 import Title from "./Title";
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <div className="App">
       <Title />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImage={setSelectedImage} />
+      {selectedImage && (
+        <Modal
+          selectedImage={selectedImage}
+          setSelectedImage={setSelectedImage}
+        />
+      )}
     </div>
   );
 }
--- src/Components/ImageGrid.js
+++ src/Components/ImageGrid.js
@@ -1,15 +1,29 @@
 import React from "react";
 import useFirestore from "../Hooks/useFirestore";
-function ImageGrid() {
+import { motion } from "framer-motion";
+
+function ImageGrid({ setSelectedImage }) {
   const { docs } = useFirestore("images");
 
   return (
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div className="img-wrap" key={doc.id}>
-            <img src={doc.url} alt="Uploaded image" />
-          </div>
+          <motion.div
+            className="img-wrap"
+            key={doc.id}
+            onClick={() => setSelectedImage(doc.url)}
+            whileHover={{ opacity: 1 }}
+            layout
+          >
+            <motion.img
+              src={doc.url}
+              alt="Uploaded image"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
+          </motion.div>
         ))}
     </div>
   );
